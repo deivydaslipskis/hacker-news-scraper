97b9064f7ed223cbf6d4b8bf4a14f7bb3dcae6fc
deivydaslipskis <deivydaslipskis@gmail.com>
2021-06-20 16:52:54 +0300

    create function to get links, titles and points from HN

--- scrape.py
+++ scrape.py
@@ -1,6 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 response = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(response.text, 'html.parser')
+selected_links = soup.select('.storylink')
+selected_subtexts = soup.select('.subtext')
 
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for index, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points >= 100:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+
+pprint.pprint(create_custom_hn(selected_links, selected_subtexts))
